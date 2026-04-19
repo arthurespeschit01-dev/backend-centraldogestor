62bf6be072ca805caeae6742ff0f173c684980b3
arthurespeschit01-dev <arthurespeschit01@gmail.com>
2026-04-19 20:45:39 -0300

    Update check-access.js

--- api/check-access.js
+++ api/check-access.js
@@ -1,13 +1,36 @@
 import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+  "https://jgynkqdgawknmgwkusls.supabase.co",
+  process.env.SUPABASE_SERVICE_KEY
+);
+
 export default async function handler(req, res) {
   try {
-    const supabase = createClient(
-      "https://jgynkqdgawknmgwkusls.supabase.co", // <- VAMOS FORÇAR
-      process.env.SUPABASE_SERVICE_KEY
-    );
+    const { email } = req.query;
 
-    return res.status(200).json({ ok: true });
+    if (!email) {
+      return res.status(400).json({ access: false });
+    }
+
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .single();
+
+    if (error || !user) {
+      return res.json({ access: false });
+    }
+
+    const now = new Date();
+    const expires = new Date(user.expires_at);
+
+    if (user.status === "active" && now <= expires) {
+      return res.json({ access: true });
+    }
+
+    return res.json({ access: false });
 
   } catch (err) {
     return res.status(500).json({ error: err.message });
